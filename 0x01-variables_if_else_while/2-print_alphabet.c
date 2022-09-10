27bd0ad045a5e9233ec43e58073895abd4b4a474
Damilarry <damilarry46@gmail.com>
2022-09-10 01:53:25 -0700

    Task 2 is to commit

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -11,10 +11,10 @@ int main(void)
 
 	for (alphabet = 'a';  alphabet <= 'z'; alphabet++)
 {
-	putchar(alphabet);
+	putchar (alphabet);
 }
 
-        putchar('\n');
+        putchar ('\n');
 
 	return (0);
 
