84dcb642f3d19ebe7b5ef684bbf4ec2bcbe5b7f0
Damilarry <damilarry46@gmail.com>
2022-09-10 08:53:54 -0700

    Task 8 is review to commit

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,8 +9,8 @@ int main(void)
 	char base_16;
 	
 	for (base_16 = 48; base_16 <= 57; base_16++)
-		(putchar(base_16);
-		 }
+		putchar(base_16);
+		 
 
 		 for (base_16 = 97; <= 102; base_16++)
 		 {
