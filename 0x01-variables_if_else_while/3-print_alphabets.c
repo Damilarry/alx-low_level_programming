8c57432afe3b42c0cd9409d6bddb4343bc5003f1
Damilarry <damilarry46@gmail.com>
2022-09-10 04:52:34 -0700

    Task 3 adjust to commit

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -12,17 +12,18 @@ int main(void)
 
 	for (lower_case = 'a'; lower_case <= 'z'; lower_case++)
         {
-		putchar(lower_case);
+	putchar(lower_case);
 	}
 
 	for (upper_case = 'A'; upper_case <= 'z'; upper_case++)
 	{
-		putchar(upper_case);
+	putchar(upper_case);
 	}
 
 
 
-	putchar('\n');
+	putchar ('\n');
 
 	return (0);
+
 }
