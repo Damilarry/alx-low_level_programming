54bbf89fc3bdebb4a41c7c37c2c730d7d2915db7
Damilarry <damilarry46@gmail.com>
2022-09-13 08:53:41 -0700

    Task 0 can be commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,16 +6,16 @@
  */
 int main(void)
 {
-        putchar('_');
-	putchar('p');
-        putchar('u');
-	putchar('t');
-	putchar('c');
-	putchar('h');
-	putchar('a');
-	putchar('r');
-	putchar('\n');
 
- 
-	 return (0);
+putchar('_');
+putchar('p');
+putchar('u');
+putchar('t');
+putchar('c');
+putchar('h');
+putchar('a');
+putchar('r');
+putchar('\n');
+
+return (0);
 }
