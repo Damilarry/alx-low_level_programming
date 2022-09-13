216ead990c6839eb94e1736eb44dce612d869ac1
Damilarry <damilarry46@gmail.com>
2022-09-13 14:46:19 -0700

    Task can be commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,18 +2,19 @@
 #include <stdlib.h>
 #include <string.h>
 #include "main.h"
-
 /**
  * print_alphabet -> prints the lowercase alphabets
  */
 
-void print_alphabet(void);
+void print_alphabet(void)
 {
+
 int j;
 
 for (j = 'a'; j <= 'z'; j++)
 {
 putchar(j);
 }
-putchar('\n');
+putchar('\n')
+
 }
