8a540a2af0358cbc5b5d16777b6c53d21a0289aa
Damilarry <damilarry46@gmail.com>
2022-09-13 09:51:44 -0700

    Tassk 1 can be commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,7 +3,7 @@
 /**
  * main - Entry point
  * 
- * Description - print the alphabet, in lowercase using only _putchar
+ * Description - print the alphabet, in lowercase using only putchar
  *
  * Return: Always 0 (Success)
  */
@@ -17,6 +17,6 @@ while (c <= 'z')
 putchar(c);
 c++;
 
-
 putchar('\n');
+
 }
