86c084e7a506c2df03a10786c8e5c66a4ad256a3
Damilarry <damilarry46@gmail.com>
2022-09-14 03:56:49 -0700

    Task 4 to commmit

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -7,7 +7,7 @@
  *
  * Return: returns 0 or 1 depending on condition
  */
-int _isalpha(int c);
+int _isalpha(int c)
 {
-return ((c >= 'a'  && c <= 'z') || (c >= 'A' && C <= 'Z'));
+return ((c >= 'a'  && c <= 'z') || (c >= 'A' && C <= 'Z'))
 }
