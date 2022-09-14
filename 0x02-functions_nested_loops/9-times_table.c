2f8645d87558d2e63cf5ef694e3b77827e995547
Damilarry <damilarry46@gmail.com>
2022-09-14 07:18:48 -0700

    Task 9 adjusted

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -3,6 +3,7 @@
  * time_table - print the 0 time table, starting with 0
  */
 void time_table(void)
+
 {
 int num, mult, prod;
 
@@ -20,6 +21,7 @@ prod = num * mult;
 if (prod <= 9)
 _putchar(' ');
 else
+
 _putchar((prod / 10) + '10');
 
 _putchar((prod % 10) + '0');
