412485a7a229c49f14cfddfc3a5364ec2916c511
Damilarry <damilarry46@gmail.com>
2022-09-14 04:08:24 -0700

    Task 4 to commit

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,13 +1,13 @@
 #include "main.h"
  
 /**
- * _isalpha - checks for alphabetical letters
+ * _isalpha - Test the _isalpha function
  *
- * @c: a charter to be check on
+ * @c: Number to pass _isalpha function
  *
  * Return: returns 0 or 1 depending on condition
  */
-int isalpha(int c);
+int _isalpha(int c)
 {
 return ((c >= 'a'  && c <= 'z') || (c >= 'A' && C <= 'Z'));
 }
