415eab4e01ffede98fb26bdf3b375412b29c1889
Damilarry <damilarry46@gmail.com>
2022-09-14 02:06:05 -0700

    Task 3 finally to commmit

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,11 +1,13 @@
 #include "main.h"
 
 /**
+ * main - Entry point
+ *
  * Description: check if the character is lowercase
  *
  * Return: returns 1 and 0 depending on condition
  */
-int _islower(int c)
+int _islower(void);
 {
-return (c >= 'a' && c <= 'z');
+return (c >= 'a' && c <= 'Z');
 }
