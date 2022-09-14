8961f9c4450734914a44df69b5755d192c691851
Damilarry <damilarry46@gmail.com>
2022-09-13 23:44:06 -0700

    Ready task to commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,10 +5,10 @@
 /**
  * print_alphabet -> prints the lowercase alphabets
  */
-void print_alphabet(void);
-
-int j;
+void print_alphabet:(void);
 {
+int j;
+
 for (j = 'a'; j <= 'z'; j++)
 
 putchar(j);
