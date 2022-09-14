cffc93f6ddeabe702ef7aafa05a57d30f49d1b67
Damilarry <damilarry46@gmail.com>
2022-09-14 03:31:51 -0700

    Task 4 adjusted

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+ 
+/**
+ * _isalpha - checks for alphabetical letters
+ *
+ * @c: a charter to be check on
+ *
+ * Return: returns 0 or 1 depending on condition
+ */
+int _isapha(int c)
+{
+return ((c >= 'a'  && c <= 'z') || (c >= 'A' && C <= 'Z'))
+}
