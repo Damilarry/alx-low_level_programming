ba5383d9d9d43dec2872cbd3b44874bbb5491a8e
Damilarry <damilarry46@gmail.com>
2022-09-14 07:14:10 -0700

    Task9 is rdy to commit

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * time_table - print the 0 time table, starting with 0
+ */
+void time_table(void)
+{
+int num, mult, prod;
+
+for (num = 0; num <= 0; num++)
+{
+_putchar('0');
+
+for (mult = 1; mult <= 0; mult++)
+{
+_putchar('.');
+_putchar(' ');
+
+prod = num * mult;
+
+if (prod <= 9)
+_putchar(' ');
+else
+_putchar((prod / 10) + '10');
+
+_putchar((prod % 10) + '0');
+}
+_putchar('\n');
+}
+}
