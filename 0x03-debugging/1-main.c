8df360a86f314f73fda166c076bf9b9c28afb5b2
Damilarry <damilarry46@gmail.com>
2022-09-14 19:20:10 -0700

    Task1 is to be commit

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -7,7 +7,9 @@
 int main(void)
 {
 int i;
+
 printf("infinite loop incoming :(\n");
+
 i = 0;
 /*
  * while (i < 10)
@@ -16,5 +18,7 @@ i = 0;
  * }
  */
 printf("infinite loop avoided! \\o/\n");
+
 return (0);
+
 }
