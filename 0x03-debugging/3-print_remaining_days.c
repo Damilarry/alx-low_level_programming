701933a171a12ac9f2e340b47224a616035d49e8
Damilarry <damilarry46@gmail.com>
2022-09-15 03:06:26 -0700

    Tas3 adjusted for commit

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -7,7 +7,6 @@
  * @year: year
  * Return: void
  */
-
 void print_remaining_day(int month, int day, int year);
 {
 if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0))
@@ -23,9 +22,13 @@ printf("Remaining days: %d\n", 366 - day);
 else
 {
 if (month == 2 && day == 60)
+{
 printf("Invalid date: %02d/%2d/%04d\n", month, day - 31, year);
 }
 else
+{
 printf("Day of the year: %d\n", day);
-print("Remaining days: %d\n", 365 - day);
+printf("Remaining days: %d\n", 365 - day);
+}
+}
 }
