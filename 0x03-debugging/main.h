918155efc41ef7de3df6968da70456c1e2c89d70
Damilarry <damilarry46@gmail.com>
2022-09-14 18:44:47 -0700

    Task0 is to commit

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_h
+
+#include <stdio.h>
+
+void postitive_or_negative(int i);
+void 2-largest_number.c, (int, int, int);
+void print_remaining_days(int, int, int)
+int convert_day(int, int);
+
+#endif /* MAIN_H */
