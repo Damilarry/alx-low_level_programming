5db524e6a6d9282e0425d51ad95e2fa36b5a69ef
Damilarry <damilarry46@gmail.com>
2022-09-14 20:22:41 -0700

    Task2 to be commit

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -4,7 +4,7 @@
 #include <stdio.h>
 
 void postitive_or_negative(int i);
-void 2-largest_number.c, (int, int, int);
+void largest_number(int, int, int);
 void print_remaining_days(int, int, int)
 int convert_day(int, int);
 
