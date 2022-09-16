7e63fb8e50a39b3a64ea01c2be2059ec855e33d6
Damilarry <damilarry46@gmail.com>
2022-09-16 05:35:14 -0700

    Task4 adjusted to commit

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,13 +2,14 @@
 #include "main.h"
 /**
  * print_most_numbers - prints numbers except 2 and 4
+ *
  * Return: return nothing
  */
 void print_most_numbers(void)
 {
 int n;
 
-for (n = 46; n < 58; n++)
+for (n = 48; n < 58; n++)
 {
 if ((n == 50) || (n == 52))
 {
