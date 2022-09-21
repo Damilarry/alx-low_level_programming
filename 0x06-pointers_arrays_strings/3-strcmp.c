0a3772d973033783377ef223e3d3736c136e9143
Damilarry <Damilarry46@gmail.com>
2022-09-21 14:09:22 -0700

    Task3 to commit

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,38 +1,20 @@
 #include "main.h"
 /**
- * _strcop - Function which compare two strings and
- * @s1: first string
- * @s2: second string
- * Return:
- *                 return zero if s1 == s2
- *                 return nagative number if s1 < s2
- *                 return positive number if s1 > s2
- */
-int _strcop(char *s1, char *s2)
+ * * _strspn -search n display length
+*
+*@s: stores the input
+*@accept: source
+*Return: comp
+*/
+unsigned int _strspn(char *s, char *accept)
 {
-int i = 0, diff = 0;
-
-while (1)
+unsigned int i, j;
+for (i = 0; i < s[i] !='\0'; i++)
 {
-if (s1[i] == '\0' && s2[i] == '\0')
-break;
-else if (s1[i] == '\0')
+for (j = 0; i < accept[j] !='\0'; j++)
 {
-diff = s2[i];
-break;
+if (*s == *accept)
+return accept[j];
 }
-else if (s2[i] == '\0')
-{
-diff = s1[i];
-break;
 }
-else if (s1[i] != s2[i])
-{
-diff = s1[i] - s2[i];
-break;
-}
-else
-i++;
-}
-return (diff);
 }
