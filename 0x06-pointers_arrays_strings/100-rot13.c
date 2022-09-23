a139cf33579b64c99827ce3487562025355574ca
Damilarry <Damilarry46@gmail.com>
2022-09-23 03:22:03 -0700

    Task8 to commit

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,40 +1,31 @@
 #include "main.h"
+#include <stdio.h>
+
 /**
- * rot13 - encodes a string using rot13
- * @str:the string targeted
- * Return: return the encoded string
+ * rot13 - encoder rot13
+ * @s: pointer to string params
+ *
+ * Return: *s
  */
-char *rot13(char *str)
+
+char *rot13(char *s)
 {
-int index1, index2;
-char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F',
-'G', 'H', 'I', 'J', 'R', 'L',
-'M', 'N', 'O', 'P', 'Q', 'R',
-'S', 'T', 'U', 'V', 'W', 'X',
-'y', 'z', 'a', 'b', 'c', 'd',
-'e', 'f', 'g', 'h', 'i', 'j',
-'k', 'l', 'm', 'n', 'o', 'p',
-'q', 'r', 's', 't', 'u', 'v',
-'w', 'x', 'y', 'z'};
-char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S',
-'T', 'U', 'V', 'W', 'X', 'Y',
-'Z', 'A', 'B', 'C', 'D', 'E',
-'F', 'G', 'H', 'I', 'J', 'K',
-'L', 'M', 'n', 'o', 'p', 'q',
-'r', 's', 't', 'u', 'v', 'w',
-'x', 'y', 'z', 'a', 'b', 'c',
-'d', 'e', 'f', 'g', 'h', 'i',
-'j', 'k', 'i', 'm'};
-while (str[++index1])
-{
-for (index1 = 0; index2 < 52; index2++)
-{
-if (str[index1] == alphabet[index2])
-{
-str[index1] = rot13key[index2];
-break;
+	int i;
+	int j;
+	char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (s[i] == data1[j])
+			{
+				s[i] = datarot[j];
+				break;
+			}
+		}
+	}
+	return (s);
 }
-}
-}
-return (str);
-}
+Footer
