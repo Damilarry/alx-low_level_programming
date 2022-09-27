edaf1194ff90648538a5e7f58a882bb3ea48efa7
Damilarry <Damilarry46@gmail.com>
2022-09-27 02:17:35 -0700

    Task0 adjusted

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -5,16 +5,16 @@
  *@b: the desired value
  *@n: number of bytes to be changed
  *
- *Return: changed array with new value for n bytes
+ *Return: new string.
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	int i = 0;
+	unsigned int i = 0;
 
-	for (; n > 0; i++)
+	while (i < n)
 	{
-		s[i] = b;
-		n--;
+	        *(s + i) = b;
+		i++;
 	}
 	return (s);
 }
