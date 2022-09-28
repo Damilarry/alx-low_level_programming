2c46b1e84a347525029fb5ffbae24adce4b4cf1f
Damilarry <Damilarry46@gmail.com>
2022-09-28 12:18:30 -0700

    Task0

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,9 +1,9 @@
 #ifndef MAIN_H
-#define MAIN.H
+#define MAIN_H
 
 #include <stdio.h>
 
-int -putchar(char c);
+int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
